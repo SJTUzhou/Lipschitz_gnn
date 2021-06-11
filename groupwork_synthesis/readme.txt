cf50f75dbef953eecd1dbe801ee86b803640bb04
CHEN Yizhou <yizhou.chen@student-cs.fr>
2021-06-11 14:09:58 +0100

    re-structure

--- groupwork_synthesis/readme.txt
+++
@@ -1,3 +0,0 @@
-Hello, 
-this is a collection of codes that we have written
-you just need execute codes in this file 
--- gt/test.py
+++
@@ -1,7 +0,0 @@
-# a = {1:2,3:4}
-# k =[3,4]
-# c =[i for i in a.keys() if i not in k ]
-# a ={i:i+3 for i in range(2)}
-# print(a)
-import numpy as np
-p = np.random.choice([1 ,2 ,3], p = [0.5, 0.5, 0])
